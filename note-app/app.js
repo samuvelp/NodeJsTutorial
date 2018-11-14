e8d3863860d3de4d59bd207e11c9103bde80cb27
Samuvel P <samuvelpandian@gmail.com>
2018-11-15 00:04:06 +0530

    Note app day2- writing json to file started

--- note-app/app.js
+++ note-app/app.js
@@ -3,21 +3,17 @@ const _ = require('lodash')
 const yargs = require('yargs')
 const note = require('./note.js')
 
-const command  = process.argv[2]
 const argvYarg = yargs.argv
-
-console.log(`command: ${command}`)
-console.log("Process: ",process.argv)
-console.log("Yarg: ",argvYarg)
+const command  = argvYarg._[0]
 
 if(_.isEqual(command,"add")){
     note.addNote(argvYarg.title,argvYarg.body)
-}else if(_.isEqual(command,"read")){
-    console.log("Reading all notes")
 }else if(_.isEqual(command,"list")){
-    console.log("Listing all notes")
+    note.listNote(argvYarg.title)
+}else if(_.isEqual(command,"read")){
+    note.readNote(argvYarg.title)
 }else if(_.isEqual(command,"remove")){
-    console.log("Removing note")
+    note.removeNote(argvYarg.title)
 }else{
     console.log("Command not recognised")
 }
--- note-app/note.js
+++ note-app/note.js
@@ -1,6 +1,36 @@
+const fs = require('fs')
 var addNote = (title,body)=>{
-    console.log("Adding Note",title,body)
+    var notes = []
+    var note = {
+        title,
+        body
+    }
+    try{
+    var notesString = fs.readFileSync("notes-data.json")
+    notes = JSON.parse(notesString)
+    }catch(e){
+        console.log(e)
+    }  
+    var duplicateString = notes.filter((note)=>{
+        return note.title === title
+    })
+    if(duplicateString.length===0){
+        notes.push(note)
+        fs.writeFileSync("notes-data.json",JSON.stringify(notes))
+    }  
+}
+var listNote = (title)=>{
+    console.log("Listing Note",title)
+}
+var readNote = (title)=>{
+    console.log("Reading notes",title)
+}
+var removeNote = (title)=>{
+    console.log("Removing note",title)
 }
 module.exports ={
-    addNote
+    addNote,
+    listNote,
+    readNote,
+    removeNote
 }
